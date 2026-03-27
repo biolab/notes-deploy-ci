27e3814f017ea4f2ff3a5ace79a7f41b185db8f9
janezd <janez.demsar@fri.uni-lj.si>
2026-03-27 21:05:59 +0100

    Link checker: checkout with ssh key

--- .github/scripts/check-links.js
+++ .github/scripts/check-links.js
@@ -26,17 +26,17 @@ const CONCURRENCY = 10;
           let response;
           try {
             if (!sourceUrl) {
-              response = await page.goto(url, { timeout: 30000 });
+              response = await page.goto(url, { waitUntil: "networkidle", timeout: 30000 });
             } else {
-              response = await page.request.head(url);
-              console.log(url, response.ok(), response.status());
+              response = await page.context().request.head(url);
             }
           }
           catch (err) {
-            //results[url] = [err.message ? err.message.split('\n')[0] : 'Unknown Error', false, sourceUrl];
-            results[url] = ["CONN", false, sourceUrl];
+            results[url] = [err.message ? err.message.split('\n')[0] : 'Unknown Error', false, sourceUrl];
+            //results[url] = ["CONN", false, sourceUrl];
             continue;
           }
+          console.log(url, response.ok(), response.status());
           results[url] = [response.status(), response.ok(), sourceUrl];
           if (response.ok()) {
             const extractedUrls = await page.evaluate(() => {
