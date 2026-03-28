7d2299b0218a77ac03dc1132e78c1cef8782a5b7
janezd <janez.demsar@fri.uni-lj.si>
2026-03-28 09:38:27 +0100

    Link checker: checkout with ssh key

--- .github/scripts/check-links.js
+++ .github/scripts/check-links.js
@@ -2,95 +2,96 @@ import { chromium } from 'playwright';
 import * as fs from 'fs';
 
 const CONCURRENCY = 1;
+const blockedDomains = [
+  'youtube.com',
+  '4beta.orangedatamining.com',
+  'youtu.be',
+  'googletagmanager.com',
+  'google-analytics.com',
+];
 
 
 (async () => {
-  const urlsToCheck = process.argv.slice(2).map((url) => [url]);
+
+  const urlsToCheck = process.argv.slice(2);
+  let urlPtr = 0;
+  const getNextUrl = async () =>
+    await navigator.locks.request(
+      'crawler-queue',
+      async () => urlPtr < urlsToCheck.length ? urlsToCheck[urlPtr++] : null);
+
   const results = {};  // key is target url, value [status, ok (true/false), source-url]
 
   const browser = await chromium.launch();
   //const browser = await chromium.launch({ headless: false });
 
   const worker = async () => {
-      while (urlsToCheck.length > 0) {
-        const [url, sourceUrl] = urlsToCheck.shift();
-        if (url in results) {
-          continue;
-        }
-        else {
-          results[url] = [];  // assign yourself to this url
-        }
-
-        const page = await browser.newPage();
+    for(let url; url = await getNextUrl(); ) {
+      const page = await browser.newPage();
+      const probe = async (url, sourceUrl) => {
+        results[url] = ["PROCESSING"];
         try {
-          let response;
-          try {
-            if (!sourceUrl) {
-              await page.route('**/*', (route) => {
-                const url = route.request().url();
-                const type = route.request().resourceType();
-                if (type === 'subframe' || url.includes('youtube.com')) {
-                  return route.abort();
-                }
-                route.continue();
-              });
-              const blockedDomains = [
-                  'youtube.com',
-                  '4beta.orangedatamining.com',
-                  'youtu.be',
-                  'googletagmanager.com',
-                  'google-analytics.com',
-                ];
-
-                const isBlockedDomain = blockedDomains.some(domain => url.includes(domain));
-                if (isBlockedDomain) {
-                  return route.abort();
-                }
-              page.on('request', request => console.log(`🚀 Requesting: [${request.resourceType()}] ${request.url()}`));
-
-              // (Optional) Print when it finishes so you can see what is STUCK
-              page.on('requestfinished', request => console.log(`✅ Finished: ${request.url()}`));
-              page.on('requestfailed', request => console.log(`❌ Failed: ${request.url()}`));
-              response = await page.goto(url, { waitUntil: "networkidle", timeout: 30000 });
-            } else {
-              response = await page.context().request.head(url);
-            }
-          }
-          catch (err) {
-            results[url] = [err.message ? err.message.split('\n')[0] : 'Unknown Error', false, sourceUrl];
-            //results[url] = ["CONN", false, sourceUrl];
-            continue;
-          }
-
+          const response = await page.context().request.head(url);
           results[url] = [response.status(), response.ok(), sourceUrl];
-          if (response.ok()) {
-            const extractedUrls = await page.evaluate(() => {
-              const links = Array.from(document.querySelectorAll('a')).map(a => a.href);
-              const imgs = Array.from(document.querySelectorAll('img')).map(img => img.src);
-              return [...links, ...imgs]
-                 .filter(url => typeof url === 'string' && url.trim().startsWith("http"))
-                 .map(url => {
-                      try {
-                        const parsed = new URL(url);
-                        parsed.hash = '';
-                        return decodeURI(parsed.toString());
-                      } catch {
-                        return null;
-                      }
-                    })
-                .filter(Boolean);
-            });
-            if (!sourceUrl) { // don't recurse
-              urlsToCheck.push(...extractedUrls.map(newUrl => [newUrl, url]));
-            }
-          }
-        } catch (error) {
-          results[url] = ['FAILED/TIMEOUT', false, sourceUrl];
-        } finally {
-          await page.close();
+          return response.ok();
+        }
+        catch (err) {
+          results[url] = [err.message ? err.message.split('\n')[0] : 'Unknown Error', false, sourceUrl];
+          //results[url] = ["CONN", false, sourceUrl];
+          return false;
         }
       }
-    };
+
+      if (!await probe(url)) {
+        continue;
+      }
+
+      await page.route('**/*', (route) => {
+        const url = route.request().url();
+        const type = route.request().resourceType();
+        if (type === 'subframe' || blockedDomains.some(domain => url.includes(domain))) {
+          return route.abort();
+        }
+        return route.continue();
+      });
+
+      try {
+        const response = await page.goto(url, { waitUntil: "networkidle", timeout: 5000 });
+        if (response.ok()) {
+          const extractedUrls = await page.evaluate(() => {
+            const links = Array.from(document.querySelectorAll('a')).map(a => a.href);
+            const imgs = Array.from(document.querySelectorAll('img')).map(img => img.src);
+            return [...links, ...imgs]
+              .filter(url => typeof url === 'string' && url.trim().startsWith("http"))
+              .map(url => {
+                try {
+                  const parsed = new URL(url);
+                  if (parsed.protocol !== 'http:' && parsed.protocol !== 'https:') {
+                    return null;
+                  }
+                  parsed.hash = '';
+                  return parsed.href;
+                } catch(e){
+                  results[url] = ["MALFORMED", false, sourceUrl];
+                  return null;
+                }
+              })
+              .filter(Boolean);
+          });
+          console.log(extractedUrls);
+          for(const subUrl of extractedUrls) {
+            await probe(subUrl, url);
+          }
+        }
+      } catch (err) {
+        if (err.name !== 'TimeoutError') {
+          results[url] = ['FAILED', false, sourceUrl];
+        }
+      } finally {
+        await page.close();
+      }
+    }
+  }
 
   const workerPool = [];
   const activeWorkers = Math.min(CONCURRENCY, urlsToCheck.length);
